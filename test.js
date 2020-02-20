bb9b39b21df57a841e00efaede392f447036f984
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-02-20 14:39:13 +0100

    Create

---
+++ test.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const cu = require('./lib/confutils')
+
+cu.setEnvDir(path.join(__dirname, "env"))
+
+cu.writeFileToConfig("common", "default", "SACCKEY", path.join(__dirname, "env/sacckeyorig.json"))
+
+cu.writeMakeEnvScript([["common", "default"]], "makeenv.bat")
+
+cu.fromChunksToFile("SACCKEY", path.join(__dirname, "env/sacckey.json"))
